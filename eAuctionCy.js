73d86541953fae62ca1b7210807c03e4e9112720
mirek11s <miroslavsabev11@gmail.com>
2022-12-24 12:25:20 +0200

    finishing the eAuction script

--- eAuctionCy.js
+++ eAuctionCy.js
@@ -36,175 +36,193 @@ import { delay, getDateString } from "./constants.js";
     await englishLangBtn.click();
     await page.waitForNavigation(); // Wait for the page to load after changing lang
 
-    await page.waitForSelector("#AuctionsListDiv .AList-BoxContainer");
-    const auctionsContainer = await page.$$("#AuctionsListDiv .AList-BoxContainer");
-
-    for (const auction of auctionsContainer) {
-      let date_of_conduct = "";
-      let date_posted = "";
-      let city = "";
-      let district = "";
-      let unique_code = "";
-      let reserved_price = "";
-      let status = "";
-      let real_estate_type = "";
-      let area_sq_m = "";
-      let mortgage_lender_name = "";
-      let notification_date = "";
-
+    let isNextBtnExist = true;
+    while (isNextBtnExist) {
       try {
-        date_of_conduct = await page.evaluate(
-          (el) =>
-            el.querySelector("div.AList-BoxMainCnt > div.AList-BoxMainCell1 > div.DateIcon")
-              .textContent,
-          auction
-        );
-      } catch (e) {
-        console.log(e);
+        await page.waitForSelector("#AuctionsListDiv .AList-BoxContainer");
+      } catch (error) {}
+      const auctionsContainer = await page.$$("#AuctionsListDiv .AList-BoxContainer");
+
+      for (const auction of auctionsContainer) {
+        let date_of_conduct = "";
+        let date_posted = "";
+        let city = "";
+        let district = "";
+        let unique_code = "";
+        let reserved_price = "";
+        let status = "";
+        let real_estate_type = "";
+        let area_sq_m = "";
+        let mortgage_lender_name = "";
+        let notification_date = "";
+
+        try {
+          date_of_conduct = await page.evaluate(
+            (el) =>
+              el.querySelector("div.AList-BoxMainCnt > div.AList-BoxMainCell1 > div.DateIcon")
+                .textContent,
+            auction
+          );
+        } catch (e) {
+          console.log(e);
+        }
+
+        try {
+          const location = await page.evaluate(
+            (el) =>
+              el.querySelector(
+                "div.AList-BoxMainCnt > div.AList-BoxMainCell4 > div.AList-BoxTextBlue"
+              ).textContent,
+            auction
+          );
+          const [townUnfiltered, neighbourhood] = location.split("Municipality");
+          const [, town] = townUnfiltered.split(": ");
+          city = town;
+          district = neighbourhood;
+        } catch (e) {}
+
+        try {
+          const diryPriceStr = await page.evaluate(
+            (el) => el.querySelector("div.AList-Boxheader > div.AList-BoxheaderRight").textContent,
+            auction
+          );
+          const clearedStr = diryPriceStr.replace(/\s+/g, " ").trim();
+          const [, newPrice] = clearedStr.split(": ");
+          reserved_price = newPrice.replace(/\./g, ",");
+        } catch (e) {
+          console.log(e);
+        }
+
+        //////////////////////////////////////////////////
+        // find the link in the offer advert and then open newTab using it
+        const contentLink = await page.evaluate((offer) => {
+          const anchor = offer.querySelector(
+            "div.AList-BoxFooter > div > div.AList-BoxFooterRight > a"
+          );
+          const href = anchor.getAttribute("href");
+          return href;
+        }, auction);
+
+        const page2 = await page.browser().newPage();
+        try {
+          await page2.goto(`https://www.eauction-cy.com${contentLink}`, {
+            timeout: 160000,
+          });
+          await page2.bringToFront();
+          try {
+            await page2.waitForSelector("#PropertyArea");
+          } catch (error) {
+            console.log(error);
+          }
+          try {
+            const uniqueCodeElement = await page2.$(".AuctionDetailsDivR .ADetailsinput");
+            unique_code = await page2.evaluate((el) => el.textContent, uniqueCodeElement);
+          } catch (error) {
+            console.log(error);
+          }
+          try {
+            date_posted = await page2.evaluate(() => {
+              const dirtyDate = document.querySelector(".ADetailsinputDateOn").textContent;
+              // specifies the pattern of the date in the string
+              const dateRegex = /\d{2}\/\d{2}\/\d{4}/;
+              const date = dirtyDate.match(dateRegex)[0];
+              return date;
+            });
+          } catch (error) {
+            console.log(error);
+          }
+          try {
+            status = await page2.evaluate(() => {
+              const div = document.querySelector(".StateValue");
+              return div.textContent;
+            });
+          } catch (error) {
+            console.log(error);
+          }
+          try {
+            real_estate_type = await page2.evaluate(() => {
+              return document.querySelector(
+                "div.AuctionDetailsSection1 > div:nth-child(8) > label.ADetailsinput"
+              ).textContent;
+            });
+          } catch (error) {
+            console.log(error);
+          }
+          try {
+            area_sq_m = await page2.evaluate(() => {
+              return document.querySelector("#PropertyArea").textContent;
+            });
+          } catch (error) {
+            console.log(error);
+          }
+          try {
+            mortgage_lender_name = await page2.evaluate(() => {
+              const dirtyLabel = document.querySelector(".ADetailsinput2Cell");
+              const label = dirtyLabel.textContent;
+              return label.replace(/\s+/g, " ").trim();
+            });
+          } catch (error) {
+            console.log(error);
+          }
+          try {
+            notification_date = await page2.evaluate(() => {
+              return document.querySelector("#publishDate").textContent;
+            });
+          } catch (error) {
+            console.log(error);
+          }
+          const scrapedData = {
+            date_of_conduct,
+            date_posted,
+            city,
+            district,
+            unique_code,
+            reserved_price,
+            status,
+            real_estate_type,
+            area_sq_m,
+            mortgage_lender_name,
+            notification_date,
+          };
+          // filter the list to check if current unique_code already exist and if it does, dont push it to avoid duplicates
+          const existsInList = list.some((obj) => obj.unique_code === unique_code);
+          if (!existsInList) {
+            list.push(scrapedData);
+            const jsonList = JSON.stringify(scrapedData) + ",";
+            fs.appendFileSync(`eAuctionCy_${date}.json`, jsonList, function (err) {
+              if (err) throw err;
+            });
+          }
+          await delay(3000);
+          await page2.close();
+          await delay(3000);
+        } catch {
+          await page2.close();
+        }
       }
 
       try {
-        const location = await page.evaluate(
-          (el) =>
-            el.querySelector(
-              "div.AList-BoxMainCnt > div.AList-BoxMainCell4 > div.AList-BoxTextBlue"
-            ).textContent,
-          auction
+        const element = await page.$("div.AList-GridPageNext");
+        // Get the execution context for the element
+        const executionContext = await element.executionContext();
+        // Scroll the element into view
+        await executionContext.evaluate(
+          (element) => element.scrollIntoView({ behavior: "smooth", block: "end" }),
+          element
         );
-        const [townUnfiltered, neighbourhood] = location.split("Municipality");
-        const [, town] = townUnfiltered.split(": ");
-        city = town;
-        district = neighbourhood;
-      } catch (e) {}
+        await element.hover();
 
-      try {
-        const diryPriceStr = await page.evaluate(
-          (el) => el.querySelector("div.AList-Boxheader > div.AList-BoxheaderRight").textContent,
-          auction
-        );
-        const clearedStr = diryPriceStr.replace(/\s+/g, " ").trim();
-        const [, newPrice] = clearedStr.split(": ");
-        reserved_price = newPrice;
-      } catch (e) {
-        console.log(e);
-      }
+        const nextButton = await page.$("#pagerNext");
+        const styleAttribute = await page.$eval("#pagerNext", (el) => el.getAttribute("style"));
+        console.log(styleAttribute);
 
-      //////////////////////////////////////////////////
-      // find the link in the offer advert and then open newTab using it
-      const contentLink = await page.evaluate((offer) => {
-        const anchor = offer.querySelector(
-          "div.AList-BoxFooter > div > div.AList-BoxFooterRight > a"
-        );
-        const href = anchor.getAttribute("href");
-        return href;
-      }, auction);
-
-      const page2 = await page.browser().newPage();
-      try {
-        await page2.goto(`https://www.eauction-cy.com${contentLink}`, {
-          timeout: 160000,
-        });
-
-        await page2.bringToFront();
-        try {
-          await page2.waitForSelector("#PropertyArea");
-        } catch (error) {
-          console.log(error);
+        isNextBtnExist = styleAttribute.includes("pointer");
+        if (isNextBtnExist) {
+          await nextButton.click();
+          await delay(4000);
         }
-
-        try {
-          const uniqueCodeElement = await page2.$(".AuctionDetailsDivR .ADetailsinput");
-          unique_code = await page2.evaluate((el) => el.textContent, uniqueCodeElement);
-        } catch (error) {
-          console.log(error);
-        }
-
-        try {
-          date_posted = await page2.evaluate(() => {
-            const dirtyDate = document.querySelector(".ADetailsinputDateOn").textContent;
-            // specifies the pattern of the date in the string
-            const dateRegex = /\d{2}\/\d{2}\/\d{4}/;
-            const date = dirtyDate.match(dateRegex)[0];
-            return date;
-          });
-        } catch (error) {
-          console.log(error);
-        }
-
-        try {
-          status = await page2.evaluate(() => {
-            const div = document.querySelector(".StateValue");
-            return div.textContent;
-          });
-        } catch (error) {
-          console.log(error);
-        }
-
-        try {
-          real_estate_type = await page2.evaluate(() => {
-            return document.querySelector(
-              "div.AuctionDetailsSection1 > div:nth-child(8) > label.ADetailsinput"
-            ).textContent;
-          });
-        } catch (error) {
-          console.log(error);
-        }
-
-        try {
-          area_sq_m = await page2.evaluate(() => {
-            return document.querySelector("#PropertyArea").textContent;
-          });
-        } catch (error) {
-          console.log(error);
-        }
-
-        try {
-          mortgage_lender_name = await page2.evaluate(() => {
-            const dirtyLabel = document.querySelector(".ADetailsinput2Cell");
-            const label = dirtyLabel.textContent;
-            return label.replace(/\s+/g, " ").trim();
-          });
-        } catch (error) {
-          console.log(error);
-        }
-
-        try {
-          notification_date = await page2.evaluate(() => {
-            return document.querySelector("#publishDate").textContent;
-          });
-        } catch (error) {
-          console.log(error);
-        }
-
-        const scrapedData = {
-          date_of_conduct,
-          date_posted,
-          city,
-          district,
-          unique_code,
-          reserved_price,
-          status,
-          real_estate_type,
-          area_sq_m,
-          mortgage_lender_name,
-          notification_date,
-        };
-
-        // filter the list to check if current unique_code already exist and if it does, dont push it to avoid duplicates
-        const existsInList = list.some((obj) => obj.unique_code === unique_code);
-        if (!existsInList) {
-          list.push(scrapedData);
-          const jsonList = JSON.stringify(scrapedData) + ",";
-          fs.appendFileSync(`eAuctionCy_${date}.json`, jsonList, function (err) {
-            if (err) throw err;
-          });
-        }
-
-        await delay(3000);
-        await page2.close();
-        await delay(3000);
-      } catch {
-        await page2.close();
+      } catch (error) {
+        console.log(error);
       }
     }
   });
