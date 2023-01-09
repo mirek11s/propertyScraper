3d00d8a230c6cd3fd96cacf64f6c363aaa280775
mirek11s <miroslavsabev11@gmail.com>
2023-01-09 19:39:48 +0200

    test1

--- utils/countLength.js
+++ utils/countLength.js
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 
 // Read the JSON file
-const data = JSON.parse(fs.readFileSync("../bazaraki_retails_20230103.json", "utf8"));
+const data = JSON.parse(fs.readFileSync("../bazaraki_retails_20230108.json", "utf8"));
 
 // Determine the length of the array
 const length = data.length;
